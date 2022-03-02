ab5d455f78ff292239bbf8efb38d1fa01ce22fe9
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-02 13:34:52 +0500

    integrating xml view into compose

--- app/src/main/java/xyz/teamgravity/composexmlinteroperability/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/composexmlinteroperability/MainActivity.kt
@@ -1,14 +1,22 @@
 package xyz.teamgravity.composexmlinteroperability
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.constraintlayout.widget.ConstraintLayout
 import xyz.teamgravity.composexmlinteroperability.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +38,23 @@ class MainActivity : AppCompatActivity() {
                         textAlign = TextAlign.Center,
                         fontSize = 20.sp
                     )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    AndroidView(
+                        modifier = Modifier.fillMaxWidth(),
+                        factory = { context ->
+                            TextView(context)
+                        }) { textView ->
+                        textView.apply {
+                            textView.layoutParams = ConstraintLayout.LayoutParams(
+                                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                                ConstraintLayout.LayoutParams.WRAP_CONTENT
+                            )
+                            text = "This is a TextView inside compose :)"
+                            textSize = 20F
+                            setTextColor(Color.BLACK)
+                            textAlignment = View.TEXT_ALIGNMENT_CENTER
+                        }
+                    }
                 }
             }
         }
