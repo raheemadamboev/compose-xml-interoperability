c2529e5f51f4bb4413544793ee748bcb80222f41
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-02 13:27:17 +0500

    integrating compose into xml

--- app/src/main/java/xyz/teamgravity/composexmlinteroperability/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/composexmlinteroperability/MainActivity.kt
@@ -1,11 +1,37 @@
 package xyz.teamgravity.composexmlinteroperability
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import xyz.teamgravity.composexmlinteroperability.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = "Hello Compose Text!",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
     }
 }
